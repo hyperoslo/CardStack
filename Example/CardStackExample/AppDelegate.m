4ad8bde6bc420b440af9a11b87e065fce8473093
Attila Bardos <attila@velorum.hu>
2015-04-16 13:34:27 +0200

    Update example

--- Example/CardStackExample/AppDelegate.m
+++ Example/CardStackExample/AppDelegate.m
@@ -1,24 +1,29 @@
 #import "AppDelegate.h"
 #import "CardStackController.h"
+#import "Masonry.h"
 
 static const NSUInteger ExampleNumberOfInitialCards = 4;
+static const CGFloat ExampleButtonWidth = 320.0f;
+static const CGFloat ExampleButtonHeight = 44.0f;
+static const CGFloat ExampleTopMargin = 64.0f;
+static const CGFloat ExampleMargin = 10.0f;
 
 @interface AppDelegate () <CardStackControllerDelegate>
 
 @property (nonatomic) CardStackController *cardStackController;
 @property (nonatomic) NSMutableArray *viewControllers;
+@property (nonatomic) NSUInteger numberOfCardsCreated;
 
 @end
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
 
     self.viewControllers = [NSMutableArray new];
     for (NSUInteger i = 0; i < ExampleNumberOfInitialCards; i++) {
-        [self.viewControllers addObject:[self createNewTestViewController]];
+        [self.viewControllers addObject:[self createNewTestViewControllerWithTag:i]];
     }
 
     self.cardStackController = [CardStackController new];
@@ -41,20 +46,91 @@ static const NSUInteger ExampleNumberOfInitialCards = 4;
 
 #pragma mark - CardStackControllerDelegate
 
-- (void)cardStackControllerWillOpen:(CardStackController *)cardStackController
-{
+- (void)cardStackControllerWillOpen:(CardStackController *)cardStackController {
     CardView *card = [self.cardStackController.cards lastObject];
-    card.title = [NSString stringWithFormat:@"#%ld", (long)self.cardStackController.cards.count];
+    if ([card.title isEqualToString:@"Tap to open stack"]) {
+        card.title = [NSString stringWithFormat:@"#%ld", (long)self.cardStackController.cards.count];
+    }
 }
 
 #pragma mark - Other methods
 
-- (UIViewController *)createNewTestViewController {
+- (UIViewController *)createNewTestViewControllerWithTag:(NSInteger)tag {
     UIViewController *viewController = [[UIViewController alloc] init];
 
     viewController.view.backgroundColor = [UIColor whiteColor];
+    viewController.view.tag = tag;
+
+    UIButton *insertAboveButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, ExampleButtonWidth, ExampleButtonHeight)];
+    [insertAboveButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    [insertAboveButton setTitle:@"Insert card above" forState:UIControlStateNormal];
+    [insertAboveButton addTarget:self action:@selector(insertAboveAction:) forControlEvents:UIControlEventTouchUpInside];
+    [viewController.view addSubview:insertAboveButton];
+    [insertAboveButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(viewController.view.mas_top).with.offset(ExampleTopMargin);
+        make.centerX.equalTo(viewController.view.mas_centerX);
+    }];
+
+    UIButton *insertBelowButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, ExampleButtonWidth, ExampleButtonHeight)];
+    [insertBelowButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    [insertBelowButton setTitle:@"Insert card below" forState:UIControlStateNormal];
+    [insertBelowButton addTarget:self action:@selector(insertBelowAction:) forControlEvents:UIControlEventTouchUpInside];
+    [viewController.view addSubview:insertBelowButton];
+    [insertBelowButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(insertAboveButton.mas_bottom).with.offset(ExampleMargin);
+        make.centerX.equalTo(viewController.view.mas_centerX);
+    }];
+
+    UIButton *removeButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, ExampleButtonWidth, ExampleButtonHeight)];
+    [removeButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    [removeButton setTitle:@"Remove this card" forState:UIControlStateNormal];
+    [removeButton addTarget:self action:@selector(removeAction:) forControlEvents:UIControlEventTouchUpInside];
+    [viewController.view addSubview:removeButton];
+    [removeButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(insertBelowButton.mas_bottom).with.offset(ExampleMargin);
+        make.centerX.equalTo(viewController.view.mas_centerX);
+    }];
+
+    self.numberOfCardsCreated = self.numberOfCardsCreated + 1;
 
     return viewController;
+}
+
+- (void)insertAboveAction:(UIButton *)button {
+    NSInteger index = button.superview.tag;
+    UIViewController *aboveViewController = [self.viewControllers objectAtIndex:index];
+    UIViewController *viewController = [self createNewTestViewControllerWithTag:self.viewControllers.count];
+    [self.viewControllers insertObject:viewController atIndex:index];
+    NSString *title = [NSString stringWithFormat:@"#%ld", (long)self.numberOfCardsCreated];
+    [self.cardStackController insertCardWithViewController:viewController
+                                                 withTitle:title
+                                       aboveViewController:aboveViewController
+                                                  animated:YES
+                                            withCompletion:^{
+                                                for (NSUInteger i = 0; i < self.viewControllers.count; i++) {
+                                                    UIViewController *viewController = [self.viewControllers objectAtIndex:i];
+                                                    viewController.view.tag = i;
+                                                }
+                                                NSLog(@"self.cardStackController.cards = %@", self.cardStackController.cards);
+                                            }];
+}
+
+- (void)insertBelowAction:(UIButton *)button {
+
+}
+
+- (void)removeAction:(UIButton *)button {
+    NSInteger index = button.superview.tag;
+    [self.viewControllers removeObjectAtIndex:index];
+    [self.cardStackController removeCardAtIndex:index
+                                       animated:YES
+                                 withCompletion:^{
+                                     for (NSUInteger i = 0; i < self.viewControllers.count; i++) {
+                                         UIViewController *viewController = [self.viewControllers objectAtIndex:i];
+                                         viewController.view.tag = i;
+                                     }
+                                     NSLog(@"self.cardStackController.cards = %@", self.cardStackController.cards);
+                                 }];
 }
 
 @end
